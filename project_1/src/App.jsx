fec6260a7c252c323859c8b4e27216aa79c6e98c
Sejal Godbole <sejalgodbole02@gmail.com>
2025-01-15 16:53:40 +0000

    Project 1 completed

---
+++ project_1/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navigation from './components/Navigation.jsx'
+import HeroSection from './components/Hero.jsx'
+
+const App = () => {
+
+    return (
+        <div>
+            <Navigation />
+            <HeroSection />
+        </div>
+    )
+}
+
+export default App
